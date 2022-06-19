7c4cf100aeb91e8959fa3c98ff64826279cdf274
Максим Холуев <maxwellchaos@gmail.com>
2022-06-19 13:05:09 +0300

    Добавьте файлы проекта.

---
+++ VideoPlayerWatcher/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace VideoPlayerWatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.VerticalTo = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.HorizontalTo = new System.Windows.Forms.NumericUpDown();
+            this.HorizontalFrom = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.VerticalFrom = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.VerticalTo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HorizontalTo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HorizontalFrom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VerticalFrom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(827, 152);
+            this.panel1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.VerticalTo);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.HorizontalTo);
+            this.groupBox1.Controls.Add(this.HorizontalFrom);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.VerticalFrom);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(15, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(235, 91);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Область слежения";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 64);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(19, 13);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "до";
+            // 
+            // VerticalTo
+            // 
+            this.VerticalTo.Location = new System.Drawing.Point(62, 62);
+            this.VerticalTo.Name = "VerticalTo";
+            this.VerticalTo.Size = new System.Drawing.Size(61, 20);
+            this.VerticalTo.TabIndex = 10;
+            this.VerticalTo.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.VerticalTo.ValueChanged += new System.EventHandler(this.HorizontalTo_ValueChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "от";
+            // 
+            // HorizontalTo
+            // 
+            this.HorizontalTo.Location = new System.Drawing.Point(165, 62);
+            this.HorizontalTo.Name = "HorizontalTo";
+            this.HorizontalTo.Size = new System.Drawing.Size(61, 20);
+            this.HorizontalTo.TabIndex = 9;
+            this.HorizontalTo.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.HorizontalTo.ValueChanged += new System.EventHandler(this.HorizontalTo_ValueChanged);
+            // 
+            // HorizontalFrom
+            // 
+            this.HorizontalFrom.Location = new System.Drawing.Point(165, 36);
+            this.HorizontalFrom.Name = "HorizontalFrom";
+            this.HorizontalFrom.Size = new System.Drawing.Size(61, 20);
+            this.HorizontalFrom.TabIndex = 8;
+            this.HorizontalFrom.ValueChanged += new System.EventHandler(this.HorizontalTo_ValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(162, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Горизонталь";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // VerticalFrom
+            // 
+            this.VerticalFrom.Location = new System.Drawing.Point(62, 36);
+            this.VerticalFrom.Name = "VerticalFrom";
+            this.VerticalFrom.Size = new System.Drawing.Size(61, 20);
+            this.VerticalFrom.TabIndex = 7;
+            this.VerticalFrom.ValueChanged += new System.EventHandler(this.HorizontalTo_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Вертикаль";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label3.Location = new System.Drawing.Point(561, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.textBox1.Location = new System.Drawing.Point(596, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(231, 152);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(177, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "секунд";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(115, 23);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(54, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Проверять раз в ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 152);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(827, 301);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 453);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "VideoPlayerWatcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.VerticalTo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HorizontalTo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HorizontalFrom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VerticalFrom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown VerticalTo;
+        private System.Windows.Forms.NumericUpDown HorizontalTo;
+        private System.Windows.Forms.NumericUpDown HorizontalFrom;
+        private System.Windows.Forms.NumericUpDown VerticalFrom;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ VideoPlayerWatcher/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace VideoPlayerWatcher
+{
+    public partial class Form1 : Form
+    {
+        //Скриншоты
+        Bitmap ScreenShot;
+        Bitmap PrevScreenShot;
+
+        //Координаты начала выделения
+        int StartSelectionX;
+        int StartSelectionY;
+
+        //Заблокировать созранение параметров, пока они не загружены
+        bool BlockSave = true;
+
+        //Масштаб отображения скриншота
+        //Так же используется для выделения рамки
+        int scale = 1;
+
+        //Признак того, что кнопка мыши зажата на пикчебоксе и идет выделение
+        bool MouseDowned = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        public void MakeScreenshot()
+        {
+            //Освобождаю память от предыдущего скриншота
+            PrevScreenShot?.Dispose();
+
+            //Копирую скриншот в предыдущий
+            PrevScreenShot = ScreenShot;
+
+            //далее получаю новый скриншот
+
+            // получаем размеры виртуального экрана, содержащего все мониторы
+            Rectangle bounds = SystemInformation.VirtualScreen;
+
+            // создаем пустое изображения размером с экран устройства
+            ScreenShot = new Bitmap(bounds.Width, bounds.Height);
+            // создаем объект на котором можно рисовать
+            using (var g = Graphics.FromImage(ScreenShot))
+            {
+                // перерисовываем экран на наш графический объект
+                g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);
+            }
+
+            //Устанавливаю ограничение на Вертикальные и горизонтальные числа
+            VerticalTo.Maximum = ScreenShot.Height;
+            HorizontalTo.Maximum = ScreenShot.Width;
+            VerticalFrom.Maximum = ScreenShot.Height;
+            HorizontalFrom.Maximum = ScreenShot.Width;
+        }
+
+        public bool CompareScreenShots()
+        {
+            //Если скриншота нет, то он не меняется
+            if(PrevScreenShot == null)
+                return false;
+
+            //Сравниваю все пиксели 
+            for (int i = (int)HorizontalFrom.Value; i < (int)HorizontalTo.Value; i++)
+                for (int j = (int)VerticalFrom.Value; j < (int)VerticalTo.Value; j++)
+                {
+                    //сравниваю два пикселя с разных скриншотов
+                    if(ScreenShot.GetPixel(i,j)!=PrevScreenShot.GetPixel(i,j))
+                    {
+
+                        return true;
+                    }
+                }
+            return false;
+        }
+
+        //показать скриншот виртуального экрана
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (ScreenShot != null)
+            {
+                //Вычислить оптимальные размеры
+                float horizontal = ScreenShot.Width / pictureBox1.Width;
+                float vertical = ScreenShot.Height / pictureBox1.Height;
+                scale = (int)Math.Max(horizontal, vertical)+1;
+
+                //Нарисовать уменьшенный скриншот и рамку выделения
+                e.Graphics.DrawImage(ScreenShot, 0, 0, ScreenShot.Width / scale, ScreenShot.Height / scale);
+                e.Graphics.DrawRectangle(Pens.Black,
+                    (int)HorizontalFrom.Value/scale, (int)VerticalFrom.Value / scale, 
+                    (int)(HorizontalTo.Value-HorizontalFrom.Value) / scale, (int)(VerticalTo.Value-VerticalFrom.Value) / scale);
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            //Изменено значение интервала
+            //изменяем значение таймера
+            timer1.Interval = (int)numericUpDown1.Value*1000;
+
+            SaveValues();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Сделать скриншот
+            MakeScreenshot();
+
+            //Сравнить скриншоты и показать результат
+            if (CompareScreenShots())
+            {
+                textBox1.AppendText(DateTime.Now.TimeOfDay.ToString() + " Видео идет\r\n");
+                label3.Text = "Видео идет";
+            }
+            else
+            {
+                textBox1.AppendText(DateTime.Now.TimeOfDay.ToString() + " Видео остановлено\r\n");
+                label3.Text = "Видео остановлено";
+            }
+
+            //Перерисовать скриншот
+            pictureBox1.Invalidate();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            try
+            {
+                //Задаю начальные значения
+                StartSelectionX = e.X;
+                StartSelectionY = e.Y;
+                VerticalFrom.Value = e.Y * scale;
+                HorizontalFrom.Value = e.X * scale;
+                VerticalTo.Value = e.Y * scale;
+                HorizontalTo.Value = e.X * scale;
+                //началось выделение
+                MouseDowned = true;
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            MouseDowned = false;
+            SaveValues();
+        }
+
+        private void pictureBox1_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            //Еси не идет выделение, но ничего не делаем
+            if (!MouseDowned)
+                return;
+            //if (Mouse.LeftButton == MouseButtonState.Released)
+            //    return;
+
+            try
+            {
+                if(e.Y>StartSelectionY)
+                {
+                    VerticalFrom.Value = StartSelectionY * scale;
+                    VerticalTo.Value = e.Y * scale;
+                }
+                else
+                {
+                    VerticalFrom.Value = e.Y * scale;
+                    VerticalTo.Value = StartSelectionY * scale; 
+                }
+                if (e.X > StartSelectionX)
+                {
+                    HorizontalFrom.Value = StartSelectionX * scale;
+                    HorizontalTo.Value = e.X * scale;
+                }
+                else
+                {
+                    HorizontalFrom.Value = e.X * scale;
+                    HorizontalTo.Value = StartSelectionX * scale;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            pictureBox1.Invalidate();
+        }
+        void SaveValues()
+        {
+            if (!BlockSave)
+            {
+                Properties.Settings.Default.Timeout = (int)numericUpDown1.Value;
+                Properties.Settings.Default.HorizontalFrom = (int)HorizontalFrom.Value;
+                Properties.Settings.Default.HorizontalTo = (int)HorizontalTo.Value;
+                Properties.Settings.Default.VerticalFrom = (int)VerticalFrom.Value;
+                Properties.Settings.Default.VerticalTo = (int)VerticalTo.Value;
+                Properties.Settings.Default.Save();
+            }
+        }
+        void LoadValues()
+        {
+            try
+            {
+                numericUpDown1.Value = Properties.Settings.Default.Timeout;
+                HorizontalFrom.Maximum = Properties.Settings.Default.HorizontalFrom;
+                HorizontalFrom.Value = Properties.Settings.Default.HorizontalFrom;
+
+                HorizontalTo.Maximum = Properties.Settings.Default.HorizontalTo;
+                HorizontalTo.Value = Properties.Settings.Default.HorizontalTo;
+
+                VerticalFrom.Maximum = Properties.Settings.Default.VerticalFrom;
+                VerticalFrom.Value = Properties.Settings.Default.VerticalFrom;
+
+                VerticalTo.Maximum = Properties.Settings.Default.VerticalTo;
+                VerticalTo.Value = Properties.Settings.Default.VerticalTo;
+            }
+            catch(Exception)
+            {
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadValues();
+            //Пусть таймер сразу тикнет
+            timer1_Tick(sender, e);
+
+            //Пусть теперь можно сохранять
+            BlockSave = false;
+        }
+
+        private void HorizontalTo_ValueChanged(object sender, EventArgs e)
+        {
+            SaveValues();
+        }
+    }
+}
